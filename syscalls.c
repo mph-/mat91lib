5cd06f8c7f62ee4898b679a465c98f08179a18c2
Michael Hayes <michael.hayes@canterbury.ac.nz>
2010-01-12 02:24:40 +0000

    Add file I/O

--- syscalls.c
+++ syscalls.c
@@ -9,42 +9,40 @@
 #include <errno.h>
 #include <unistd.h>
 
-
-static int
-stdio_write_block (void *stream, const char *buffer, int size);
-
-static int
-stdio_read_block (void *stream, char *buffer, int size);
+#include "sys.h"
 
 
+#ifndef SYS_FILE_NUM
+#define SYS_FILE_NUM 4
+#endif
+
+
+
+typedef struct sys_file_struct
+{
+    sys_dev_t *dev;
+    void *file;
+} sys_file_t;
+
+static sys_dev_t stdin_dev;
+static sys_dev_t stdout_dev;
+static sys_dev_t stderr_dev;
+
+static sys_file_t sys_files[SYS_FILE_NUM + 3] =
+{
+    {.dev = &stdin_dev, .file = 0},
+    {.dev = &stdout_dev, .file = 0},
+    {.dev = &stderr_dev, .file = 0}
+};
+static int sys_file_num = 3;
+static sys_fs_t *sys_fs;
+static void *sys_fs_arg;
 static void (*stdio_putc) (void *stream, int ch) = 0;
 static int (*stdio_getc) (void *stream) = 0;
-static int (*stdio_write) (void *stream, const char *buffer, int size) = stdio_write_block;
-static int (*stdio_read) (void *stream, char *buffer, int size) = stdio_read_block;
-static void *stdio_stream = 0;
+
+
 
 extern char _heap_start__;    /** Start of heap.  */
-
-
-void
-stdio_redirect (void (*putc1) (void *stream, int ch),
-                int (*getc1) (void *stream), void *stream)
-{
-    stdio_putc = putc1;
-    stdio_getc = getc1;
-    stdio_stream = stream;
-}
-
-
-void
-stdio_redirect_block (int (*write1) (void *stream, const char *buffer, int size),
-                      int (*read1) (void *stream, char *buffer, int size),
-                      void *stream)
-{
-    stdio_write = write1;
-    stdio_read = read1;
-    stdio_stream = stream;
-}
 
 
 /* Helper write routine if no block write exists.  */
@@ -85,46 +83,122 @@ stdio_read_block (void *stream, char *buffer, int size)
 }
 
 
-int
-_read (int file __UNUSED__, char *buffer, int size)
+/* This is an old compatibility function for character based I/O.  */
+void
+stdio_redirect (void (*putc1) (void *stream, int ch),
+                int (*getc1) (void *stream), void *stream)
 {
-    if (!stdio_read)
+    stdio_putc = putc1;
+    stdio_getc = getc1;
+    sys_redirect_stdin (stdio_read_block, stream);
+    sys_redirect_stdout (stdio_write_block, stream);
+    sys_redirect_stderr (stdio_write_block, stream);
+}
+
+
+void
+sys_redirect_stdin (sys_read_t read1, void *file)
+{
+    sys_files[0].dev->read = read1;
+    sys_files[0].file = file;
+}
+
+
+void
+sys_redirect_stdout (sys_write_t write1, void *file)
+{
+    sys_files[1].dev->write = write1;
+    sys_files[1].file = file;
+}
+
+
+void
+sys_redirect_stderr (sys_write_t write1, void *file)
+{
+    sys_files[2].dev->write = write1;
+    sys_files[2].file = file;
+}
+
+
+int
+_read (int fd, char *buffer, int size)
+{
+    if (fd >= sys_file_num || !sys_files[fd].dev->read)
     {
         errno = ENODEV;
         return -1;
     }
-    return stdio_read (stdio_stream, buffer, size);
+
+    return sys_files[fd].dev->read (sys_files[fd].file, buffer, size);
 }
 
 
 int
-_lseek (int file __UNUSED__, int ptr __UNUSED__, int dir __UNUSED__)
+_lseek (int fd __UNUSED__, int offset __UNUSED__, int dir __UNUSED__)
 {
-    return -1;
-}
-
-
-int
-_write (int file __UNUSED__, char *buffer, int size)
-{
-    if (!stdio_write)
+    if (fd >= sys_file_num || !sys_files[fd].dev->lseek)
     {
         errno = ENODEV;
         return -1;
     }
-    return stdio_write (stdio_stream, buffer, size);
+
+    return sys_files[fd].dev->lseek (sys_files[fd].file, offset, dir);
 }
 
 
 int
-_open (const char *path __UNUSED__, int flags __UNUSED__, ...)
+_write (int fd, char *buffer, int size)
 {
-    return 0;
+    if (fd >= sys_file_num || !sys_files[fd].dev->write)
+    {
+        errno = ENODEV;
+        return -1;
+    }
+
+    return sys_files[fd].dev->write (sys_files[fd].file, buffer, size);
 }
 
 
 int
-_close (int file __UNUSED__)
+_open (const char *path, int flags, ...)
+{
+    void *arg;
+    int fd;
+
+    /* This is not called for stdin, stdout, stderr.  */
+
+    for (fd = 3; fd < SYS_FILE_NUM + 3; fd++)
+    {
+        if (!sys_files[fd].dev)
+            break;
+    }
+    if (fd == SYS_FILE_NUM + 3)
+    {
+        errno = ENFILE;
+        return -1;
+    }
+
+    if (!sys_fs)
+    {
+        errno = EACCES;
+        return -1;
+    }
+
+    arg = sys_fs->dev->open (sys_fs_arg, path, flags);
+    if (!arg)
+    {
+        errno = EACCES;
+        return -1;
+    }
+    sys_files[fd].dev = sys_fs->dev;
+    sys_files[fd].file = arg;
+
+    return fd;
+}
+
+
+int
+_close (int fd __UNUSED__)
 {
     return 0;
 }
@@ -180,7 +254,7 @@ _sbrk (int incr)
 
 
 int
-_fstat (int file __UNUSED__, void *st __UNUSED__)
+_fstat (int fd __UNUSED__, void *st __UNUSED__)
 {
     return -1;
 }
@@ -246,4 +320,11 @@ _rename (const char *oldpath __UNUSED__, const char *newpath __UNUSED__)
 {
     errno = ENOSYS;
     return -1;
+}
+
+
+void sys_fs_register (sys_fs_t *fs, void *arg)
+{
+    sys_fs = fs;
+    sys_fs_arg = arg;
 }
