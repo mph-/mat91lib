ce188c6900473c5bf2418f8b3eedc57709ffd41d
Michael Hayes <michael.hayes@canterbury.ac.nz>
2011-03-16 03:25:59 +0000

    Tweak comments

--- usart/usart.h
+++ usart/usart.h
@@ -16,33 +16,39 @@ typedef usart_dev_t *usart_t;
 #define USART_BAUD_DIVISOR(BAUD_RATE) USART0_BAUD_DIVISOR(BAUD_RATE)
 
 
-extern usart_t
+/* Initialise UART for desired channel, baud rate, etc.  */
+usart_t
 usart_init (uint8_t channel,
             uint16_t baud_divisor);
 
 
-/* Return non-zero if there is a character ready to be read.  */
-extern bool
+/** Return non-zero if there is a character ready to be read without blocking.  */
+bool
 usart_read_ready_p (usart_t usart);
 
 
-/* Return non-zero if a character can be written without blocking.  */
-extern bool
+/** Return non-zero if a character can be written without blocking.  */
+bool
 usart_write_ready_p (usart_t usart);
 
 
-/* Read character.  This blocks.  */
-extern int8_t
+/** Read character.  This blocks.  */
+int8_t
 usart_getc (usart_t usart);
 
 
-/* Write character.  This blocks.  */
-extern int8_t
+/** Write character.  This blocks until character written to transmit buffer.  */
+int8_t
 usart_putc (usart_t usart, char ch);
 
 
-/* Write string.  This blocks.  */
-extern int8_t
+/** Write string.  This blocks until last character written to transmit buffer.  */
+int8_t
 usart_puts (usart_t usart, const char *str);
+
+
+/* Shutdown UART to save power.  */
+void
+usart_shutdown (usart_t usart);
 
 #endif
