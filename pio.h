4f50e616f0eb78dfaa1a61393bbcfd42a2dee809
Michael Hayes <michael.hayes@canterbury.ac.nz>
2011-09-04 22:14:04 +0000

    Switch pio implmentation

--- pio.h
+++ pio.h
@@ -12,32 +12,14 @@
 
 #define PIO_AT91
 
-/** Define ports.  */
+/** Define ports.  The AT91SAM7X has two PIO ports.  */
 #ifdef AT91C_BASE_PIOB
-/* The AT91SAM7X has two PIO ports.  */
-#define PORT_A AT91C_BASE_PIOA
-#define PORT_B AT91C_BASE_PIOB
+enum {PORT_A, PORT_B};
 #else
-#define PORT_A AT91C_BASE_PIOA
+enum {PORT_A};
 #endif
 
-#define PIO_A PORT_A
-#define PIO_B PORT_B
-
-
-typedef AT91S_PIO *pio_port_t;
 typedef uint32_t pio_mask_t;
-
-typedef struct
-{
-    pio_port_t port;
-    pio_mask_t bitmask;
-} pio_t;
-
-
-/* Define a PIO as a structure in terms of a port and a bitmask.  */
-#define PIO_DEFINE(PORT, PORTBIT) (pio_t){PORT, BIT (PORTBIT)}
-
 
 typedef enum pio_config_enum 
 {
@@ -55,6 +37,20 @@ typedef enum pio_config_enum
 } pio_config_t;
 
 
+typedef uint32_t pio_t;
+
+/* Define a PIO as a structure in terms of a port and a bitmask.  */
+#define PIO_DEFINE(PORT, PORTBIT) ((PORT) * 32 + BIT (PORTBIT))
+
+
+/** Private macro to lookup bitmask.  */
+#define PIO_BITMASK_(PIO) (PIO)
+
+
+/** Private macro to lookup port register.  */
+#define PIO_PORT_(PIO) (AT91C_BASE_PIOA)
+
+
 /** Configure PIO
     @param pio  */
 static inline
@@ -63,51 +59,51 @@ bool pio_config_set (pio_t pio, pio_config_t config)
     switch (config)
     {
     case PIO_OUTPUT_HIGH:
-        pio.port->PIO_SODR = pio.bitmask;
-        pio.port->PIO_PER = pio.bitmask;
-        pio.port->PIO_OER = pio.bitmask;
+        PIO_PORT_ (pio)->PIO_SODR = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_PER = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_OER = PIO_BITMASK_ (pio);
         return 1;
 
     case PIO_OUTPUT_LOW:
-        pio.port->PIO_CODR = pio.bitmask;
-        pio.port->PIO_PER = pio.bitmask;
-        pio.port->PIO_OER = pio.bitmask;
+        PIO_PORT_ (pio)->PIO_CODR = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_PER = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_OER = PIO_BITMASK_ (pio);
         return 1;
 
     case PIO_INPUT:
-        pio.port->PIO_ODR = pio.bitmask;
-        pio.port->PIO_PER = pio.bitmask;
-        pio.port->PIO_PPUDR = pio.bitmask;
+        PIO_PORT_ (pio)->PIO_ODR = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_PER = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_PPUDR = PIO_BITMASK_ (pio);
         return 1;
 
     case PIO_PULLUP:
-        pio.port->PIO_ODR = pio.bitmask;
-        pio.port->PIO_PER = pio.bitmask;
-        pio.port->PIO_PPUER = pio.bitmask;
+        PIO_PORT_ (pio)->PIO_ODR = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_PER = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_PPUER = PIO_BITMASK_ (pio);
         return 1;
 
     case PIO_PERIPH_A:
-        pio.port->PIO_ASR = pio.bitmask;
-        pio.port->PIO_PDR = pio.bitmask;
-        pio.port->PIO_PPUDR = pio.bitmask;
+        PIO_PORT_ (pio)->PIO_ASR = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_PDR = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_PPUDR = PIO_BITMASK_ (pio);
         return 1;
 
     case PIO_PERIPH_B:
-        pio.port->PIO_BSR = pio.bitmask;
-        pio.port->PIO_PDR = pio.bitmask;
-        pio.port->PIO_PPUDR = pio.bitmask;
+        PIO_PORT_ (pio)->PIO_BSR = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_PDR = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_PPUDR = PIO_BITMASK_ (pio);
         return 1;
 
     case PIO_PERIPH_A_PULLUP:
-        pio.port->PIO_ASR = pio.bitmask;
-        pio.port->PIO_PDR = pio.bitmask;
-        pio.port->PIO_PPUER = pio.bitmask;
+        PIO_PORT_ (pio)->PIO_ASR = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_PDR = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_PPUER = PIO_BITMASK_ (pio);
         return 1;
 
     case PIO_PERIPH_B_PULLUP:
-        pio.port->PIO_BSR = pio.bitmask;
-        pio.port->PIO_PDR = pio.bitmask;
-        pio.port->PIO_PPUER = pio.bitmask;
+        PIO_PORT_ (pio)->PIO_BSR = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_PDR = PIO_BITMASK_ (pio);
+        PIO_PORT_ (pio)->PIO_PPUER = PIO_BITMASK_ (pio);
         return 1;
 
     default:
@@ -121,7 +117,7 @@ bool pio_config_set (pio_t pio, pio_config_t config)
 static inline
 void pio_output_high (pio_t pio)
 {
-    pio.port->PIO_SODR = pio.bitmask;
+    PIO_PORT_ (pio)->PIO_SODR = PIO_BITMASK_ (pio);
 }
 
 
@@ -130,7 +126,7 @@ void pio_output_high (pio_t pio)
 static inline
 void pio_output_low (pio_t pio)
 {
-    pio.port->PIO_CODR = pio.bitmask;
+    PIO_PORT_ (pio)->PIO_CODR = PIO_BITMASK_ (pio);
 }
 
 
@@ -150,7 +146,7 @@ void pio_output_set (pio_t pio, bool state)
 static inline
 bool pio_output_get (pio_t pio)
 {
-    return (pio.port->PIO_ODSR & pio.bitmask) != 0;
+    return (PIO_PORT_ (pio)->PIO_ODSR & PIO_BITMASK_ (pio)) != 0;
 }
 
 
@@ -160,7 +156,7 @@ bool pio_output_get (pio_t pio)
 static inline
 bool pio_input_get (pio_t pio)
 {
-    return (pio.port->PIO_PDSR & pio.bitmask) != 0;
+    return (PIO_PORT_ (pio)->PIO_PDSR & PIO_BITMASK_ (pio)) != 0;
 }
 
 
