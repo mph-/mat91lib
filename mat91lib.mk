718ce73b815247bf0c83cb0c4ada86d555fb2e29
Michael Hayes <michael.hayes@canterbury.ac.nz>
2016-05-29 17:58:09 +1200

    Fix dependencies

--- mat91lib.mk
+++ mat91lib.mk
@@ -50,7 +50,7 @@ CXX = $(TOOLCHAIN)-g++
 LD = $(TOOLCHAIN)-g++
 OBJCOPY = $(TOOLCHAIN)-objcopy
 SIZE = $(TOOLCHAIN)-size
-DEL = rm
+DEL = rm -f
 
 
 INCLUDES += -I.
@@ -69,9 +69,12 @@ CSRC = $(filter %.c, $(SRC))
 #CCSRC = $(filter %.cc %.cpp, $(SRC))
 CCSRC = $(filter %.cpp, $(SRC))
 
+OBJDIR = objs
+DEPDIR = deps
+
 # Create list of object and dependency files.  Note, sort removes duplicates.
-OBJS = $(sort $(addprefix objs/, $(notdir $(sort $(CSRC:.c=.o) $(CCSRC:.cpp=.o)))))
-DEPS = $(sort $(addprefix deps/, $(notdir $(sort $(CSRC:.c=.d) $(CCSRC:.cpp=.d)))))
+OBJS = $(sort $(addprefix $(OBJDIR)/, $(notdir $(sort $(CSRC:.c=.o) $(CCSRC:.cpp=.o)))))
+DEPS = $(sort $(addprefix $(DEPDIR)/, $(notdir $(sort $(CSRC:.c=.d) $(CCSRC:.cpp=.d)))))
 SRC_DIRS = $(sort $(dir $(SRC)))
 
 
@@ -79,7 +82,7 @@ VPATH += $(SRC_DIRS)
 
 include $(MAT91LIB_DIR)/peripherals.mk
 
-EXTRA_OBJS = objs/crt0.o objs/mcu.o
+EXTRA_OBJS = $(OBJDIR)/crt0.o $(OBJDIR)/mcu.o
 
 
 print-deps:
@@ -91,59 +94,93 @@ print-objs:
 print-src-dirs:
 	@echo $(SRC_DIRS)
 
-objs:
-	mkdir -p objs
+$(OBJDIR):
+	mkdir -p $(OBJDIR)
 
-deps:
-	mkdir -p deps
+$(DEPDIR):
+	mkdir -p $(DEPDIR)
 
-objs/%.o: %.cpp Makefile
+$(OBJS): | $(OBJDIR)
+
+$(DEPS): | $(DEPDIR)
+
+ifeq (1, 0)
+# Strategy 1 for automatic dependency generation.  Before the .c files
+# are compiled, dependency files (.d) are created (one for each .c
+# file) using the -MM compiler option.  sed is used to add the .d file
+# as a target (in addition to the .o file) so that .d file is
+# re-created if any of the pre-requisites are modified.  While sed is
+# a standard Unix utility, it is not always available on Windows.
+# This is slightly faster than strategy 2 since the .d files are only
+# created when a pre-requisite is modified.
+
+# Rule to compile .cpp file to .o file.
+$(OBJDIR)/%.o: %.cpp Makefile
+	$(CXX) -c $(CXXFLAGS) $< -o $@
+
+# Rule to compile .c file to .o file.
+$(OBJDIR)/%.o: %.c Makefile
+	$(CC) -c $(CFLAGS) $< -o $@
+
+# Rule to create .d file from .cpp file.
+$(DEPDIR)/%.d: %.cpp
+	@set -e; $(DEL) $@; \
+	$(CC) -MM $(CXXFLAGS) $< > $@.$$$$; \
+	sed 's,\($*\)\.o[ :]*,$(OBJDIR)/\1.o $@ : ,g' < $@.$$$$ > $@; \
+	$(DEL) $@.$$$$
+
+# Rule to create .d file from .c file.
+$(DEPDIR)/%.d: %.c
+	@set -e; $(DEL) $@; \
+	$(CC) -MM $(CFLAGS) $< > $@.$$$$; \
+	sed 's,\($*\)\.o[ :]*,$(OBJDIR)/\1.o $@ : ,g' < $@.$$$$ > $@; \
+	$(DEL) $@.$$$$
+else
+# Strategy 2 for automatic dependency generation.  Dependency files
+# are included if they exist.  When a .c file is compiled, a
+# dependency file (.d) is created.  This is slightly slower than
+# strategy 1 since the .d file is regenerated whenever a file
+# compiled.
+
+$(OBJDIR)/%.o: %.cpp Makefile
 	$(CXX) -c $(CXXFLAGS) $< -o $@
 # Generate dependencies to see if object file needs recompiling.
-	$(CXX) -MM $(CXXFLAGS) $< > deps/$*.d
+	@echo -n "$(OBJDIR)/" > deps/$*.d
+	$(CXX) -MM $(CXXFLAGS) $< >> deps/$*.d
 
-objs/%.o: %.c Makefile
+$(OBJDIR)/%.o: %.c Makefile
 	$(CC) -c $(CFLAGS) $< -o $@
 # Generate dependencies to see if object file needs recompiling.
-	$(CC) -MM $(CFLAGS) $< > deps/$*.d
+	@echo -n "$(OBJDIR)/" > deps/$*.d
+	$(CC) -MM $(CFLAGS) $< >> deps/$*.d
+endif
 
 # Link object files to form output file.
-$(TARGET): deps objs $(OBJS) $(EXTRA_OBJS)
+$(TARGET): $(DEPDIR) $(OBJS) $(EXTRA_OBJS)
 	$(LD) $(OBJS) $(EXTRA_OBJS) $(LDFLAGS) -o $@ -lm -Wl,-Map=$(TARGET_MAP),--cref
 	$(SIZE) $@
 
 # Include the dependency files.
 -include $(DEPS)
 
-
-# Remove the objs directory
-clean-objs:
-	-$(DEL) -fr objs
-
-# Rebuild the code, don't delete dependencies.
-rebuild: clean-objs $(TARGET)
-
-# Generate cscope tags file
-.PHONY: cscope
-cscope:
-	cscope -Rb $(INCLUDES)
-
 # Remove non-source files.
 .PHONY: clean
 clean: 
-	-$(DEL) -f *.o *.out *.hex *.bin *.elf *.d *.lst *.map *.sym *.lss *.cfg *.ocd *~
-	-$(DEL) -fr objs deps
-
+	-$(DEL) *.o *.out *.hex *.bin *.elf *.d *.lst *.map *.sym *.lss *.cfg *.ocd *~
+	-$(DEL) -r $(OBJDIR) $(DEPDIR)
 
 # Program the device.
+.PHONY: program
 program: $(TARGET)
 	$(TOOLCHAIN)-gdb -batch -x $(SCRIPTS)/program.gdb $^
 
 # Reset the device.
+.PHONY: reset
 reset: $(TARGET)
 	$(TOOLCHAIN)-gdb -batch -x $(SCRIPTS)/reset.gdb $^
 
 # Attach debugger.
+.PHONY: debug
 debug:
 	$(TOOLCHAIN)-gdb  -x $(SCRIPTS)/debug.gdb $(TARGET)
 
