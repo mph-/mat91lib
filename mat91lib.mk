d1650cf318f313824ed93924bf76f55e58167f48
Michael Hayes <michael.hayes@canterbury.ac.nz>
2016-09-05 15:34:05 +1200

    Link with gcc

--- mat91lib.mk
+++ mat91lib.mk
@@ -44,12 +44,10 @@ include $(MAT91LIB_DIR)/$(FAMILY)/$(FAMILY).mk
 SCRIPTS = $(MAT91LIB_DIR)/$(FAMILY)/scripts
 LDSCRIPTS = $(MAT91LIB_DIR)/$(FAMILY)
 
-ifndef LD
 # To keep things simple, don't like with g++.  This is only
 # needed if use c++ libraries.
 #LD = $(TOOLCHAIN)-g++
 LD = $(TOOLCHAIN)-gcc
-endif
 
 
 CC = $(TOOLCHAIN)-gcc
