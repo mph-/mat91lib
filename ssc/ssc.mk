fce9f537b27e4cab0a60862bb7d5ad85d702f9c2
Michael Hayes <michael.hayes@canterbury.ac.nz>
2018-03-24 23:08:59 +1300

    Change hack to find ring.c

--- ssc/ssc.mk
+++ ssc/ssc.mk
@@ -1,7 +1,7 @@
 SSC_DIR = $(MAT91LIB_DIR)/ssc
 
 VPATH += $(SSC_DIR)
-INCLUDES += -I$(SSC_DIR) -I$(SSC_DIR)/../../mmculib/utility/
+INCLUDES += -I$(SSC_DIR) -I$(SSC_DIR)/../../mmculib/ring/
 
 SRC += ssc.c ssc_dma.c bssc.c ring.c
 
