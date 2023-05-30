de01a45ef3aa0c9f254a8a0dfaaf67e862a2d3c8
Michael Hayes <michael.hayes@canterbury.ac.nz>
2023-05-31 08:42:40 +1200

    Define -mno-unaligned-access

--- sam4s/sam4s.mk
+++ sam4s/sam4s.mk
@@ -1,6 +1,6 @@
 # -g3 saves macro information
 CFLAGS += -mcpu=cortex-m4 -Wall -Wstrict-prototypes -W -g3 -D$(RUN_MODE) $(sort $(INCLUDES)) \
-$(OPT) -mthumb -D__$(MCU)__ -D__SAM4S__ -Wno-unused -Wno-unused-parameter -DARM_MATH_CM4=true
+$(OPT) -mthumb -D__$(MCU)__ -D__SAM4S__ -Wno-unused -Wno-unused-parameter -DARM_MATH_CM4=true -mno-unaligned-access
 
 CXXFLAGS += -mcpu=cortex-m4 -Wall -W -g3 -D$(RUN_MODE) $(sort $(INCLUDES)) \
 $(OPT) -mthumb -D__$(MCU)__ -D__SAM4S__ -Wno-unused -Wno-unused-parameter -DARM_MATH_CM4=true -Wno-missing-field-initializers
@@ -12,5 +12,3 @@ INCLUDES += -I$(MAT91LIB_DIR)/$(FAMILY) -I$(MAT91LIB_DIR)/$(FAMILY)/atmel
 VPATH += $(MAT91LIB_DIR)/$(FAMILY)
 
 SRC += mcu_sleep.c
-
-
