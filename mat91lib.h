1eb723ca522cc2371ac196ab7b608a96bc11272a
Michael Hayes <michael.hayes@canterbury.ac.nz>
2025-02-09 20:35:07 +1300

    Add LOG2

--- mat91lib.h
+++ mat91lib.h
@@ -9,7 +9,7 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    
+
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdint.h>
@@ -53,8 +53,15 @@ extern "C" {
 #define F_CPU_UL ((unsigned long)F_CPU)
 
 
+#define LOG2_1(n) (((n) >= 2) ? 1 : 0)
+#define LOG2_2(n) (((n) >= 1 << 2) ? (2 + LOG2_1((n) >> 2)) : LOG2_1(n))
+#define LOG2_4(n) (((n) >= 1 << 4) ? (4 + LOG2_2((n) >> 4)) : LOG2_2(n))
+#define LOG2_8(n) (((n) >= 1 << 8) ? (8 + LOG2_4((n) >> 8)) : LOG2_4(n))
+#define LOG2(n)  (((n) >= 1 << 16) ? (16 + LOG2_8((n) >> 16)) : LOG2_8(n))
+
+
+
 #ifdef __cplusplus
 }
-#endif    
 #endif
-
+#endif
